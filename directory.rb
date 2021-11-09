c39cd5e6f6eb03f7ed056e0fb28aaa91b432540a
Chris <hereloss@googlemail.com>
2021-11-09 19:09:14 +0000

    fact, cohort input and footer message amended

--- directory.rb
+++ directory.rb
@@ -1,12 +1,16 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and their cohort separated by a space"
+  puts "After this, please input a fun fact about the student!"
+  puts "To finish, just hit return three times"
   students = []
   name = gets.chomp
+  fact = gets.chomp
   while !name.empty? do
-    students << {name: name,cohort: :november}
+    name_cohort = name.split(" ")
+    students << {name: name_cohort[0],cohort: name_cohort[1],facts: fact}
     puts "Now we have #{students.count} students"
     name = gets.chomp
+    fact = gets.chomp
   end
   students
 end
@@ -17,15 +21,23 @@ def print_header
 end
 
 def print(names)
-  names.each_with_index do |student,idx|
-    if (student[:name][0] != "a") && (student[:name].length < 12)
-      puts "#{idx + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+  i = 0
+  lengths = names.length
+  if names.length != 0
+    names.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort) - fun fact: #{student[:facts]}"
     end
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count > 1
+    puts "Overall, we have #{students.count} great students"
+  elsif students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "We have no students!"
+  end
 end
 
 students = input_students
